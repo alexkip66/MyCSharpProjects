21e88ed0be5d9623e1091c7f6985bc83bbb1a308
alexkip66 <alexkip@yandex.ru>
2023-08-11 20:43:44 +0300

    Выполнено домашнее задание к Уроку 1

---
+++ HomeWorks/Lesson02/Exercise13/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int num = N;
+int dig_count = 0;
+while (num != 0)
+{
+    num /= 10;
+    dig_count++;
+}
+
+if (dig_count >= 3)
+{
+    int divisor = (int)Math.Pow(10, dig_count - 3);
+    int thirdDigit = (N / divisor) % 10;
+    Console.WriteLine($"Третья цифра числа {N} - {thirdDigit}");
+}
+else 
+{
+    Console.WriteLine($"Третьей цифры в числе {N} нет.");
+}
+           
+
